5174ff0f1ab075dfc2972eca861e983a28074c4f
SIGEPAC-2022 <114256674+SIGEPAC-2022@users.noreply.github.com>
2022-12-03 00:15:42 -0600

    be
    be

--- internal/processReadBot/process/process.go
+++ internal/processReadBot/process/process.go
@@ -19,10 +19,13 @@ func NewProcessReader(log log.Logger, repository saveHistoryPaientInfo.Repositor
 }
 
 func (p processReader) ProcessReader(ctx context.Context) {
+	p.log.Log("Start process repository", constants.UUID, ctx.Value(constants.UUID))
 	respRepo, errRepo := p.repository.GetDataPersonal(ctx)
 	if errRepo != nil {
 		p.log.Log("Repo Failed", constants.UUID, ctx.Value(constants.UUID))
 	}
+
+	p.log.Log("Response Endpoint getDataHistorical", "response", respRepo, constants.UUID, ctx.Value(constants.UUID))
 
 	errService := p.service.ServiceProcessGetDataHistorical(ctx, respRepo)
 	if errService != nil {
